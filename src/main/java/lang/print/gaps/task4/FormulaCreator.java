da9d27098999e812b1ac953e19e3a3e95f111ef9
Sardorbekk <sardorbekimomaddinov@gmail.com>
2023-01-14 17:18:23 +0500

    hidd

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-     double a=2,b=3;
-     double c=(9*Math.pow(a,2.0)-5*b+2+a-7)*(a+b-4*a*b)/2;
+     int  a=2,b=3;
+     int c=(9*(a^2)-5*b+2+a-7)*(a+b-4*a*b)/2;
 
      System.out.println(c);
 
