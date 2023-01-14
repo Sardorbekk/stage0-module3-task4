4e247fcd7211661f3f3a7a5ddd2f36fe44b2702e
Sardorbekk <sardorbekimomaddinov@gmail.com>
2023-01-14 17:08:18 +0500

    hidd

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int first=63, second=68,third=61,frourth=72;
+        int first=99, second=104,third=97,frourth=114;
         System.out.println(((char) first));
         System.out.println(((char) second));
         System.out.println(((char) third));
